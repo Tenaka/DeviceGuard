0559fa8210400409ff19299c7c1cf40a5be4d48c
Tenaka <86342641+Tenaka@users.noreply.github.com>
2021-06-29 17:42:23 +0100

    Update deviceGuard-basic.ps1

--- deviceGuard-basic.ps1
+++ deviceGuard-basic.ps1
@@ -1,7 +1,7 @@
-﻿<#
+<#
 .Synopsis
 Create Device Guard Policy and enforce
-​
+
 .Description
 Device Guard has the following requirements:
 
@@ -11,35 +11,46 @@ UEFI Native Mode
 Windows 10/2016 x64
 SLAT and Virtualization Extensions (Intel VT or AMD V)
 TPM
-​
 
 Windows Features
-
     Windows Defender Application Guard (Isolation mode prior to 1703)
-
     Hyper-V Platform (Not required after 1603)
-
     Hyper-V Hypervisor
+    
+Set-RuleOption -FilePath $IntialCIPolicy -Option #Number
+    0 Enabled:UMCI	
+    1 Enabled:Boot Menu Protection	
+    2 Required:WHQL	By default
+    3 Enabled:Audit Mode (Default)	
+    4 Disabled:Flight Signing	
+    5 Enabled:Inherit Default Policy
+    6 Enabled:Unsigned System Integrity Policy (Default)
+    7 Allowed:Debug Policy Augmented
+    8 Required:EV Signers
+    9 Enabled:Advanced Boot Options Menu
+    10 Enabled:Boot Audit on Failure
+    11 Disabled:Script Enforcement
+    12 Required:Enforce Store Applications
+    13 Enabled:Managed Installer
+    14 Enabled:Intelligent Security Graph Authorization	
+    15 Enabled:Invalidate EAs on Reboot	
+    16 Enabled:Update Policy No Reboot
+    17 Enabled:Allow Supplemental Policies
+    18 Disabled:Runtime FilePath Rule Protection
+    19 Enabled:Dynamic Code Security
 
 GPO Settings
-
 Computer Configuration > Administrative Templates > System > Device Guard
-
-Turn on Virtualization Based Security (enable)
-
-Secure Boot and DMA Protection
-
-Enable Virtualization Based Protection of Code
-
-Deploy Code Integrity Policy (enable)
+    Turn on Virtualization Based Security (enable)
+        Secure Boot and DMA Protection
+    Enable Virtualization Based Protection of Code
+        Deploy Code Integrity Policy (enable)
 
 C:\DeviceGuard\SIPolicy.p7b 
-
 (C:\DeviceGuard\SIPolicy.p7b is automatically copied and converted to C:\Windows\System32\Codeintegrity\)
 
+Applies DG policy without a reboot 
 From PowerShell execute Invoke-CimMethod -Namespace root/Microsoft/Windows/CI -ClassName PS_UpdateAndCompareCIPolicy -MethodName update -Arguments @{filepath = "C:\Windows\system32\CodeIntegrity\SIPolicy.p7b"}
-
-The system will create SIPolicy.p7b and a reboot will enforce Device Guard
 
 .Version
 #>
@@ -69,5 +80,5 @@ Set-RuleOption -FilePath $IntialCIPolicy -Option 3 -delete
 #GPO is set to move SIPolicy.p7b to C:\Windows\System32\CodeIntegrity
 ConvertFrom-CIPolicy -XmlFilePath $IntialCIPolicy  -BinaryFilePath $CIPolicyBin 
 
-#Enable DG to enforce
+#Enfoces Device Guard without a reboot.
 Invoke-CimMethod -Namespace root/Microsoft/Windows/CI -ClassName PS_UpdateAndCompareCIPolicy -MethodName update -Arguments @{filepath = "C:\Windows\System32\CodeIntegrity\SIPolicy.p7b"}
